8f6595268818ce7a74f560f8b91137a096424cf7
kotonn <kotokazu68@gmail.com>
2023-02-25 00:39:35 +0900

    デザイン修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
 import { Card, Box } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
@@ -25,45 +25,46 @@ const MapContainer = () => {
       },
       () => null
     );
-    
+
   }, []);
 
 
   const mapStyles = {
     height: "80vh",
-    width: "90%"};
-    return (
-      <Box sx={{height: "100vh", width: "100vw", justifyContent: "center", alignItems: "center", background: 'linear-gradient(to bottom right, pink, lightblue)', paddingTop: '4vh'}}>
-         <Paper
-      component="form"
-      sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, marginRight: 'auto', marginLeft: 'auto'}}
+    width: "90%"
+  };
+  return (
+    <Box sx={{ height: "100vh", width: "100vw", justifyContent: "center", alignItems: "center", background: 'linear-gradient(to bottom right, pink, lightblue)', paddingTop: '4vh' }}>
+      <Paper
+        component="form"
+        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, marginRight: 'auto', marginLeft: 'auto' }}
       >
-          <InputBase
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search Google Maps"
-        inputProps={{ 'aria-label': 'search google maps' }}
-      />
-      <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-        <SearchIcon />
-      </IconButton>
-      <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-      <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
-        <DirectionsIcon />
-      </IconButton>
+        <InputBase
+          sx={{ ml: 1, flex: 1 }}
+          placeholder="Search Google Maps"
+          inputProps={{ 'aria-label': 'search google maps' }}
+        />
+        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+        <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
+          <DirectionsIcon />
+        </IconButton>
       </Paper>
-     
-    <LoadScript
-    googleMapsApiKey='AIzaSyARf2O8_kiPOjmWzrP_ZcqAdjbaT-K4uSw'>
-    <GoogleMap
-      mapContainerStyle={mapStyles}
-      zoom={13}
-      center={position}
-    >
-      <Marker position={position} />
-    </GoogleMap>
-  </LoadScript>
-  
-  </Box>
-    )   
+      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 5 }}>
+        <LoadScript
+          googleMapsApiKey='AIzaSyARf2O8_kiPOjmWzrP_ZcqAdjbaT-K4uSw'>
+          <GoogleMap
+            mapContainerStyle={mapStyles}
+            zoom={13}
+            center={position}
+          >
+            <MarkerF position={position} />
+          </GoogleMap>
+        </LoadScript>
+      </Box>
+    </Box>
+  )
 }
 export default MapContainer;

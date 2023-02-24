d0f0a8eb52245f75cdebea6d3837ed72faaef14f
kotonn <kotokazu68@gmail.com>
2023-02-25 03:12:12 +0900

    検索バー修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,61 +41,42 @@ const MapContainer = () => {
   };
 
   return (
-    <Box sx={{ height: "100vh", width: "100vw", justifyContent: "center", alignItems: "center", background: 'linear-gradient(to bottom right, pink, lightblue)', paddingTop: '4vh' }}>
-      <Paper
-        component="form"
-        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, marginRight: 'auto', marginLeft: 'auto' }}
-      >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="Search Google Maps"
-          inputProps={{ 'aria-label': 'search google maps' }}
-        />
-        <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
-          <SearchIcon />
-        </IconButton>
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
-          <DirectionsIcon />
-        </IconButton>
-      </Paper>
-      <Box sx={{ display: "flex", justifyContent: "center", marginTop: 5 }}>
-        <LoadScript
-          googleMapsApiKey='AIzaSyARf2O8_kiPOjmWzrP_ZcqAdjbaT-K4uSw' libraries={["places"]}>
+    <LoadScript
+      googleMapsApiKey='AIzaSyARf2O8_kiPOjmWzrP_ZcqAdjbaT-K4uSw' libraries={["places"]}>
+      <Box sx={{ height: "100vh", width: "100vw", justifyContent: "center", alignItems: "center", background: 'linear-gradient(to bottom right, pink, lightblue)', paddingTop: '4vh' }}>
+        <StandaloneSearchBox
+          onLoad={ref => serachBoxRef.current = ref}
+          onPlacesChanged={onPlacesChanged}
+        >
+          <Paper
+            component="form"
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400, marginRight: 'auto', marginLeft: 'auto' }}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="Search Google Maps"
+              inputProps={{ 'aria-label': 'search google maps' }}
+            />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="search">
+              <SearchIcon />
+            </IconButton>
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
+              <DirectionsIcon />
+            </IconButton>
+          </Paper>
+        </StandaloneSearchBox>
+        <Box sx={{ display: "flex", justifyContent: "center", marginTop: 5 }}>
           <GoogleMap
             mapContainerStyle={mapStyles}
             zoom={13}
             center={position}
           >
-            <StandaloneSearchBox
-              onLoad={ref => serachBoxRef.current = ref}
-              onPlacesChanged={onPlacesChanged}
-            >
-              <input
-                type="text"
-                placeholder="Customized your placeholder"
-                style={{
-                  boxSizing: `border-box`,
-                  border: `1px solid transparent`,
-                  width: `240px`,
-                  height: `32px`,
-                  padding: `0 12px`,
-                  borderRadius: `3px`,
-                  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                  fontSize: `14px`,
-                  outline: `none`,
-                  textOverflow: `ellipses`,
-                  position: "absolute",
-                  left: "50%",
-                  marginLeft: "-120px"
-                }}
-              />
-            </StandaloneSearchBox>
             <MarkerF position={position} />
           </GoogleMap>
-        </LoadScript>
+        </Box>
       </Box>
-    </Box>
+    </LoadScript>
   )
 }
 export default MapContainer;

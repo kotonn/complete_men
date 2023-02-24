f8309124cb937d4dc79031c3186edc0ad29dc718
kotonn <kotokazu68@gmail.com>
2023-02-25 02:10:00 +0900

    検索機能(仮)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript, MarkerF } from '@react-google-maps/api';
-import { Card, Box } from '@mui/material';
+import { useState, useEffect, useRef } from 'react';
+import { GoogleMap, LoadScript, MarkerF, StandaloneSearchBox } from '@react-google-maps/api';
+import { Box } from '@mui/material';
 import Paper from '@mui/material/Paper';
 import InputBase from '@mui/material/InputBase';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import DirectionsIcon from '@mui/icons-material/Directions';
 
@@ -28,11 +27,19 @@ const MapContainer = () => {
 
   }, []);
 
-
   const mapStyles = {
     height: "80vh",
     width: "90%"
   };
+
+  const serachBoxRef = useRef()
+
+  const onPlacesChanged = () => {
+    const lat = serachBoxRef.current.getPlaces()[0].geometry.location.lat();
+    const lng = serachBoxRef.current.getPlaces()[0].geometry.location.lng();
+    setPosition({ lat, lng })
+  };
+
   return (
     <Box sx={{ height: "100vh", width: "100vw", justifyContent: "center", alignItems: "center", background: 'linear-gradient(to bottom right, pink, lightblue)', paddingTop: '4vh' }}>
       <Paper
@@ -54,12 +61,36 @@ const MapContainer = () => {
       </Paper>
       <Box sx={{ display: "flex", justifyContent: "center", marginTop: 5 }}>
         <LoadScript
-          googleMapsApiKey='AIzaSyARf2O8_kiPOjmWzrP_ZcqAdjbaT-K4uSw'>
+          googleMapsApiKey='AIzaSyARf2O8_kiPOjmWzrP_ZcqAdjbaT-K4uSw' libraries={["places"]}>
           <GoogleMap
             mapContainerStyle={mapStyles}
             zoom={13}
             center={position}
           >
+            <StandaloneSearchBox
+              onLoad={ref => serachBoxRef.current = ref}
+              onPlacesChanged={onPlacesChanged}
+            >
+              <input
+                type="text"
+                placeholder="Customized your placeholder"
+                style={{
+                  boxSizing: `border-box`,
+                  border: `1px solid transparent`,
+                  width: `240px`,
+                  height: `32px`,
+                  padding: `0 12px`,
+                  borderRadius: `3px`,
+                  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                  fontSize: `14px`,
+                  outline: `none`,
+                  textOverflow: `ellipses`,
+                  position: "absolute",
+                  left: "50%",
+                  marginLeft: "-120px"
+                }}
+              />
+            </StandaloneSearchBox>
             <MarkerF position={position} />
           </GoogleMap>
         </LoadScript>

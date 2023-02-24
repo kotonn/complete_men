cd590eea627c4fecb7c3c42208f7f871c220ab59
澤木柊斗 <sawakishuto@sawakihiiragitonoMacBook-Pro.local>
2023-02-24 14:53:46 +0900

    マップ表示

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,22 @@
 import { Button } from '@mui/material';
-
-const Home = () => {
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+const MapContainer = () => {
+  const mapStyles = {        
+    height: "100vh",
+    width: "100%"};
+  const defaultCenter = {
+    lat: 41.3851, lng: 2.1734
+  }
   return (
-    <>
-    </>
+     <LoadScript
+       googleMapsApiKey='AIzaSyARf2O8_kiPOjmWzrP_ZcqAdjbaT-K4uSw'>
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={13}
+          center={defaultCenter}
+        />
+     </LoadScript>
   )
 }
-
-export default Home
+export default MapContainer;
